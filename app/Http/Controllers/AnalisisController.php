7a9c5203e2c6900c8d7455ecc920c93c6610119b
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-08-24 16:38:55 +0700

    belum selesai

--- app/Http/Controllers/AnalisisController.php
+++ app/Http/Controllers/AnalisisController.php
@@ -15,27 +15,33 @@ class AnalisisController extends Controller
     }
     public function laporan(Request $request)
     {
-        $BarangMasuk = barang_masuk::select("stok_masuk")->get();
-        $BarangPinjam = barang_pinjam::select("stok_pinjam")->get();
-        $BarangRusak = barang_rusak::select("stok_keluar")->get();
-        for ($a = 0; $a < count($BarangMasuk); $a) {
-            $masuk = $a;
-        }
-        for ($i = 0; $i < count($BarangPinjam); $i) {
-            $pinjam = $i;
-        }
-        for ($u = 0; $u < count($BarangRusak); $u) {
-            $rusak = $u;
-        }
-        return response()->json([
-            "Barang Masuk" => $masuk,
-            "Barang Pinjam" => $pinjam,
-            "Barang keluar" => $rusak
-        ]);
+        $BarangMasuk = barang_masuk::select(
+            barang_masuk::raw('SUM(stok_masuk) ')
+        )->groupBy('stok_masuk')->get();
+        $BarangPinjam = barang_pinjam::select(
+            barang_pinjam::raw('SUM(stok_pinjam) ')
+        )->groupBy('stok_pinjam')->get();
+        $BarangRusak = barang_rusak::select(
+            barang_rusak::raw('SUM(stok_keluar) ')
+        )->groupBy('stok_keluar')->get();
+        // for ($a = 0; $a < count($BarangMasuk); $a) {
+        //     $masuk = $a;
+        // }
+        // for ($i = 0; $i < count($BarangPinjam); $i) {
+        //     $pinjam = $i;
+        // }
+        // for ($u = 0; $u < count($BarangRusak); $u) {
+        //     $rusak = $u;
+        // }
         // return response()->json([
-        //     "Barang Masuk" => $BarangMasuk,
-        //     "Barang Pinjam" => $BarangPinjam,
-        //     "Barang keluar" => $BarangRusak
+        //     "Barang Masuk" => $masuk,
+        //     "Barang Pinjam" => $pinjam,
+        //     "Barang keluar" => $rusak
         // ]);
+        return response()->json([
+            "Barang Masuk" => $BarangMasuk,
+            "Barang Pinjam" => $BarangPinjam,
+            "Barang keluar" => $BarangRusak
+        ]);
     }
 }
